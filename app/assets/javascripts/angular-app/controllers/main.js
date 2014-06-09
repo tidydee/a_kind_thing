ef4b9d0bbd303c41e6d04361bf4fa7e51791246e
dawidspamer <dawidspamer@gmail.com>
2014-06-09 14:56:24 -0700

    Adding Description

--- app/assets/javascripts/angular-app/controllers/main.js
+++ app/assets/javascripts/angular-app/controllers/main.js
@@ -1,5 +1,14 @@
 angular
   .module('app')
-  .controller('MainCtrl', ['Act', '$scope', function(Act, $scope) {
+  .controller('MainCtrl', ['Act', '$scope', 
+    function(Act, $scope) {
+    
     $scope.acts = Act.query();
+    $scope.newAct = new Act();
+    $scope.save = function(act) {
+      $scope.acts.push(act);
+      act.$save();
+      $scope.newAct = new Act();
+    } 
+
   }]);
